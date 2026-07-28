066e8f7a04a47c4e7b371e1c0d8a0e3a7ddc75cd
rehansyah <ihsanreihansyah24@gmail.com>
2026-07-28 15:24:26 +0700

    update for login admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
 {
     $this->call([
         RoleSeeder::class,
+        DepartmentSeeder::class,
+        UserSeeder::class,
+        EmployeeSeeder::class,
     ]);
 }
 }
